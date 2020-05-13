494047d849ed60606d3a16ecc1d70c6eb8877e71
Luke <lukelovekin@gmail.com>
2020-05-14 08:40:51 +1000

    test deploy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users
   resources :users
-  # devise_for :users
 root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
