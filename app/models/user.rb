4a78a7fff1d3571e68d59d18a66dfa784b2aaca5
Luke <lukelovekin@gmail.com>
2020-05-15 16:52:34 +1000

    comments done?

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def full_name
-    "#{:first_name} #{:last_name}"
+    "#{first_name} #{last_name}"
   end
 end
  
