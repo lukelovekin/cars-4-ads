f946ae367e8fcbbef18597f553bf1fcdb3ce90b4
Luke <lukelovekin@gmail.com>
2020-05-21 17:30:24 +1000

    still finishing off

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,16 +48,18 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    @post.post_pics.attach(params[:post][:post_pics])
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    
+      @post.post_pics.attach(params[:post][:post_pics])
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+          format.json { render :show, status: :ok, location: @post }
+        else
+          format.html { render :edit }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
-    end
+
   end
 
   # DELETE /posts/1
